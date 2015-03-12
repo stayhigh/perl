579be41304732d6e72c21a2fee088e71c575ee37
stayhigh <stayhigh@github>
2015-03-13 01:19:00 +0800

    perl foreach oneliner and upload sample textfile

--- beauty.pl
+++ beauty.pl
@@ -3,10 +3,20 @@
 use strict;
 use warnings;
 
+#check sysargs
 my $arg_length = scalar @ARGV;
-die "usage: $0 <name>" if $arg_length != 2;
+my $PROGRAM_NAME = $0; 
+die "usage: $PROGRAM_NAME <name>" if $arg_length != 2;
 my ($filename, $times) =  @ARGV;
-open (my $FILEHANDLE, "<",$filename) or die "Could not open $filename";
+
+# show lines in file
+open (my $FILEHANDLE, "<",$filename) or die "Couldn't open $filename $!";
 my @lines = <$FILEHANDLE>;
 print @lines;
+close $FILEHANDLE;
 
+# open direcotry and show files
+opendir (DIR, '.') or die "Couldn't open directory, $!";
+my @files = readdir DIR or die "READ DIRERROR";#while (my $file = readdir DIR) { print "$file\n";}
+foreach my $file (@files) {print "$file\n";} #foreach (@files) {print "$_\n";} # print filename for each
+closedir DIR;
