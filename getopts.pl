c77d7bbffa24f2d07cacd6aa38f0c4c80f6c9e83
stayhigh <stayhigh@github>
2015-03-16 14:01:19 +0800

    getopts.pl elegant style

---
+++ getopts.pl
@@ -0,0 +1,14 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Std;
+
+#getopts
+my $opt_string = "ht:p:";
+my %opts = (); 
+getopts ("$opt_string",\%opts);
+die "-t target is a must" if not defined $opts{t};
+die "-p   port is a must" if not defined $opts{p};
+
+print "main function runs\n";
